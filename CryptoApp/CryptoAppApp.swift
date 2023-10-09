7acfec2d654a955b1a4b9008f3658d6c3c4c4ffb
LuiArm <Luisarmendariz24@icloud.com>
2023-10-09 12:28:52 -0600

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by luis armendariz on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

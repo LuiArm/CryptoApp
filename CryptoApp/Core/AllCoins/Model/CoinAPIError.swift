e27f7c56971761528bd1d7ac3f4bf1a39e0af743
LuiArm <Luisarmendariz24@icloud.com>
2023-10-16 22:39:17 -0600

    Custom error handling

--- CryptoApp/Core/AllCoins/Model/CoinAPIError.swift
+++ CryptoApp/Core/AllCoins/Model/CoinAPIError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+enum CoinAPIError: Error {
+    case invalidData
+    case jsonParsingFailure
+    case requestFailed(description: String)
+    case invalidStatusCode(statusCode: Int)
+    case unknownError(error: Error)
+    
+    //add custom description to each error case
+    var customDescription: String  {
+        switch self {
+        case .invalidData: return "Invalid data"
+        case .jsonParsingFailure: return "Failed to parse JSON"
+        case  let .requestFailed(description): return "Request failed: \(description)"
+        case let .invalidStatusCode(statusCode): return "Invalid status code: \(statusCode)"
+        case let .unknownError(error) return "An unknown error occurred: \(error.localizedDescription)"
+        }
+    }
+}
--- CryptoApp/Core/AllCoins/Service/CoinDataService.swift
+++ CryptoApp/Core/AllCoins/Service/CoinDataService.swift
@@ -9,28 +9,41 @@ import Foundation
 
 class CoinDataService {
     
-    func fetchCoinsWithResult(completion: @escaping (Result<[Coin], Error>)-> Void) {
+    func fetchCoinsWithResult(completion: @escaping (Result<[Coin], CoinAPIError>)-> Void) {
         let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
         
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) {data, response , error in
             if let error = error {
-                completion(.failure(error))
-            }
-            guard let data = data else { return }
-            
-            
-            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else {
-                print("failed to decode coins")
-                return
+                completion(.failure(.unknownError(error: error)))
             }
             
            
-            completion(.success(coins))
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(.requestFailed(description: "Request Failed")))
+                    return
+                }
+   
+                guard httpResponse.statusCode == 200 else {
+                    completion(.failure(.invalidStatusCode(statusCode: httpResponse.statusCode)))
+                    return
+                }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                completion(.success(coins))
+            } catch {
+                print("DEBUG: Failed to decode with error \(error)")
+                completion(.failure(.jsonParsingFailure))
+            }
+            
         }.resume()
-        
- 
     }
     
     
